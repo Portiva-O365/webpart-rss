6aaae602927e4923f7806fa397f28f33c1681f0e
Cuno Reijman <creijman@portiva.nl>
2019-01-19 20:22:45 +0100

    Updated rss item styling with corresponding theme colors

--- src/webparts/rss/components/Rss.module.scss.ts
+++ src/webparts/rss/components/Rss.module.scss.ts
@@ -1,18 +1,18 @@
 /* tslint:disable */
 require('./Rss.module.css');
 const styles = {
-  rss: 'rss_9240c742',
-  container: 'container_9240c742',
-  headerContainer: 'headerContainer_9240c742',
-  title: 'title_9240c742',
-  description: 'description_9240c742',
-  rssItemContainer: 'rssItemContainer_9240c742',
-  contentContainer: 'contentContainer_9240c742',
-  imageContainer: 'imageContainer_9240c742',
-  image: 'image_9240c742',
-  bulletContainer: 'bulletContainer_9240c742',
-  feedInfoContainer: 'feedInfoContainer_9240c742',
-  feedImage: 'feedImage_9240c742',
+  rss: 'rss_2c98db21',
+  container: 'container_2c98db21',
+  headerContainer: 'headerContainer_2c98db21',
+  title: 'title_2c98db21',
+  description: 'description_2c98db21',
+  rssItemContainer: 'rssItemContainer_2c98db21',
+  contentContainer: 'contentContainer_2c98db21',
+  imageContainer: 'imageContainer_2c98db21',
+  image: 'image_2c98db21',
+  bulletContainer: 'bulletContainer_2c98db21',
+  feedInfoContainer: 'feedInfoContainer_2c98db21',
+  feedImage: 'feedImage_2c98db21',
 };
 
 export default styles;
